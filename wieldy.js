1f1845c4af081de05dd35c78e930d29bf0681fc3
Seth Vandebrooke <sethvandebrooke@gmail.com>
2018-08-08 12:27:14 -0400

    Update wieldy.js

--- wieldy.js
+++ wieldy.js
@@ -1,35 +1,34 @@
+
 // (any)
 // v: any value you want to wield
-var Wieldable = function(originalValue){
-  var data, callbacks = [];
-  function broadcast() {
-    for (var i = 0; i < callbacks.length; i++) {
-      if ("function" === typeof callbacks[i]) {
-        callbacks[i](data);
+var Wieldable = function(v){
+  var d, l = [], t = () => {
+    for (var i = 0; i < l.length; i++) {
+      if (typeof l[i] === "function") {
+        l[i](d);
       }
     }
   };
   // (any)
   // v: any value you want to wield
-  var wieldable = function (value) {
-    if (value != undefined) {
-      data = value;
-      if (value instanceof Array) {
-        function methodWrapper(func, ...params) {
-          var result = data[func](...params);
-          broadcast();
-          return result;
+  var wieldable = function (v) {
+    if (v != undefined) {
+      d = v;
+      if (v instanceof Array) {
+        function arrayFunc(func, ...params) {
+          var r = d[func](...params); t();
+          return r;
         }
-        wieldable.push    =  function ( ...params ) { return methodWrapper( 'push',    ...params ); };
-        wieldable.pop     =  function ( ...params ) { return methodWrapper( 'pop',     ...params ); };
-        wieldable.shift   =  function ( ...params ) { return methodWrapper( 'shift',   ...params ); };
-        wieldable.unshift =  function ( ...params ) { return methodWrapper( 'unshift', ...params ); };
-        wieldable.splice  =  function ( ...params ) { return methodWrapper( 'splice',  ...params ); };
-        wieldable.slice   =  function ( ...params ) { return methodWrapper( 'slice',   ...params ); };
-        Object.defineProperty(wieldable, 'length',  { get: function() { return data.length; } } );
-      } else if ("object" === typeof value) {
-        for (var key in value) {
-          wieldable[key] = new Wieldable(value[key]);
+        wieldable.push    =  function ( ...params ) { return arrayFunc( 'push',    ...params ) }
+        wieldable.pop     =  function ( ...params ) { return arrayFunc( 'pop',     ...params ) }
+        wieldable.shift   =  function ( ...params ) { return arrayFunc( 'shift',   ...params ) }
+        wieldable.unshift =  function ( ...params ) { return arrayFunc( 'unshift', ...params ) }
+        wieldable.splice  =  function ( ...params ) { return arrayFunc( 'splice',  ...params ) }
+        wieldable.slice   =  function ( ...params ) { return arrayFunc( 'slice',   ...params ) }
+        Object.defineProperty(wieldable, 'length',  { get: function(){ return d.length; } });
+      } else if (typeof v === "object") {
+        for (var k in v) {
+          wieldable[k] = new Wieldable(v[k]);
         }
       } else { // Clear out helper functions if there
         if (wieldable.push)    { delete wieldable.push;    }
@@ -39,46 +38,52 @@ var Wieldable = function(originalValue){
         if (wieldable.splice)  { delete wieldable.splice;  }
         if (wieldable.slice)   { delete wieldable.slice;   }
       }
-      broadcast();
+      t();
     }
-    if (data instanceof Array) {
-      var tempArray = [];
-      for (var i = 0, callbacks = data.length; i < callbacks; i++) {
-        tempArray[i] = data[i];
-      }
-      return tempArray;
-    } else if ("object" === typeof data) {
-      var obj = {};
-      for (var key in wieldable) {
-        if ("function" === typeof wieldable[key] && "wieldable" == wieldable[key].name) {
-          obj[key] = wieldable[key]();
+    if ("object" === typeof d && !(d instanceof Array) ) {
+      var o = {};
+      for (var k in wieldable) {
+        if ("function" === typeof wieldable[k] && wieldable[k].name == "wieldable") {
+          o[k] = wieldable[k]();
         }
       }
-      return obj;
+      return o;
     }
-    return data;
+    return d;
   };
   // (function)
   // f: function to run when the value of the wieldable changes
-  wieldable.observe = function (func) {
-    if ("function" === typeof func) {
-      callbacks.push(func);
+  wieldable.observe = f => {
+    if ("function" === typeof f) {
+      l.push(f);
       return {
-        stop: function () {
-          callbacks.splice(callbacks.indexOf(func),1)
+        stop: () => {
+          l.splice(l.indexOf(f),1)
         },
-        start: function () { callbacks.push(func) }
+        start: () => l.push(f)
       };
     }
   };
-  // (DOM Element, String, Boolean)
+  // (DOM Element, String, Boolean) 
   // element: DOM Element to bind the wieldable to
   // param: Name of event (if input is true) to update on, or DOM Element property to set when updated
   // input: wheather you are setting the wieldable on an event or getting it when changed
   wieldable.bind = function (element,param,input) {
+    param = param.trim();
     if (input) {
-      element.addEventListener(param, function () {
-        wieldable(element.value);
+      if (param.replace("key","")!==param&&param.replace("-","")!==param) {
+        param = param.split("-");
+        var key = param[0];
+        param = param[1];
+      }
+      element.addEventListener(param, function(e) {
+        if (key !== undefined) {
+          if (key == e.key || key == e.code || key == e.which) {
+            wieldable(element.value);
+          }
+        } else {
+          wieldable(element.value);
+        }
       });
       if (element.value) wieldable(element.value);
       return wieldable.observe(function(text) {
@@ -86,31 +91,31 @@ var Wieldable = function(originalValue){
       });
     } else {
       wieldable.template = element[param];
-      var observer = wieldable.observe(function (text) {
-        if (data instanceof Array && wieldable.template) {
+      var observer = wieldable.observe(function(text) {
+        if (d instanceof Array && wieldable.template) {
           element[param] = "";
-          data.forEach((el,index) => {
+          d.forEach((el,index) => {
             var str = wieldable.template;
             if ("object" === typeof el) {
-              for (var key in el) {
-                str = str.split("${"+key+"}").join(el[key]);
+              for (k in el) {
+                str = str.split("${"+k+"}").join(el[k]);
               }
             } else {
+              str = str.split("${index}").join(index);
               str = str.split("${value}").join(el);
             }
-            str = str.split("${index}").join(index);
             element[param] += str;
           })
         } else {
           element[param] = text;
         }
       });
-      element[param] = data;
+      element[param] = d;
       return observer;
     }
   }
-  wieldable.changed = broadcast();
-  wieldable(originalValue);
+  wieldable.refresh = t;
+  wieldable(v);
   return wieldable;
 };
 
@@ -121,12 +126,11 @@ function WieldyScope(tar,model) {
   var scope = this;
   scope.debug = false;
   scope.run = function (target) {
-
     target.querySelectorAll("[bind]").forEach(e=> {
       var bindings = e.getAttribute("bind"), input = false;
       if (!!bindings) {
         bindings = bindings.split(",");
-        bindings.forEach(function(params){
+        bindings.forEach(function(params) {
           if (params.match(":")!==null) {
             params = params.split(":");
             var binding = params[0];
@@ -136,32 +140,50 @@ function WieldyScope(tar,model) {
                 binding = binding.replace("-on","");
                 input = true;
               }
-              if (binding.replace(".","")!=binding) {
-                binding = eval("scope."+binding);
-                if (binding === undefined) {
-                  console.error(binding+" does note exist");
-                }
-              } else {
-                binding = scope[binding] = scope[binding] || new Wieldable();
+              binding = binding.trim();
+              console.log(scope[binding],typeof scope[binding])
+              if (typeof scope[binding] === "undefined") {
+                scope[binding] = new Wieldable();
               }
               if (input) {
-                binding.bind(e,param,true);
+                if (scope[binding].name == "wieldable") {
+                  scope[binding].bind(e,param,true);
+                } else if ("function" == typeof scope[binding]) {
+                  if (param.replace("key","")!==param&&param.replace("-","")!==param) {
+                    param = param.split("-");
+                    var key = param[1];
+                    key = key.charAt(0).toUpperCase() + key.slice(1);
+                    param = param[0];
+                  }
+                  e.addEventListener(param, function(ev) {
+                    if (key !== undefined) {
+                      if (key == ev.key || key == ev.code || key == ev.which) {
+                        scope[binding](e.value);
+                      }
+                    } else {
+                      scope[binding](e.value);
+                    }
+                  });
+                }
                 scope.debug?console.log(e,param, true):null;
               } else {
-                binding(!!e[param]?e[param]:"");
-                binding.bind(e,param);
+                scope[binding](!!e[param]?e[param]:"");
+                scope[binding].bind(e,param);
                 if (e[param].match(/\$\{.*}/g) != null) {
-                  binding([]);
+                  scope[binding]([]);
+                  scope[binding].observe(function(){
+                    scope.run(e);
+                  })
                 }
                 scope.debug?console.log(e,param):null;
                 return scope;
               }
             } else {
-              console.error("No bindings were defined");
+              console.log("No bindings were defined");
               return false;
             }
           } else {
-            console.error("No bindings were defined");
+            console.log("No bindings were defined");
             return false;
           }
         })
@@ -173,7 +195,7 @@ function WieldyScope(tar,model) {
   }
   if (model) {
     for (var k in model) {
-      scope[k] = new Wieldable(model[k]);
+      scope[k] = typeof model[k] == "function" ? model[k] : new Wieldable(model[k]);
     }
   }
   if (tar) {
